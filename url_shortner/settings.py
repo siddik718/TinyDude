40457d0ed109302a97d94378aebb44b24c1d4240
Nahid Na <absiddik718@gmail.com>
2023-04-28 03:10:13 +0600

    Added PostgreSQL

--- url_shortner/settings.py
+++ url_shortner/settings.py
@@ -81,13 +81,25 @@ WSGI_APPLICATION = 'url_shortner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'a77pJSwAAlvsYjA2uKU9',
+        'HOST': 'containers-us-west-147.railway.app',
+        'PORT': '7277',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
